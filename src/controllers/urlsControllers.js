f18b4259964c6b761b9df5105089da68ad7063a8
Victormarzo <victor.marzo.monteiro@gmail.com>
2022-10-16 17:02:43 -0300

    rota de get por id funcionando

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -12,4 +12,27 @@ async function postShorten (req,res){
         console.log(error)
     }
 };
-export{postShorten}
+
+async function getUrlById (req,res){
+    const id=req.params.id;
+    try {
+        const urlValidation = (await connection.query(`
+            SELECT * 
+            FROM urls
+            WHERE id = $1; 
+            `,[id])).rows[0];
+        if(!urlValidation){
+            res.sendStatus(404);
+        }else{
+            const {id,url,shortUrl}=urlValidation;
+            const urlById={id,url,shortUrl};
+            res.status(200).send(urlById)
+        }   
+    } catch (error) {
+    console.log(error)
+    }
+};
+
+
+
+export{postShorten,getUrlById}
--- src/routers/urlsRouter.js
+++ src/routers/urlsRouter.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import authUser from '../middlewares/authMiddleware.js';
 import {urlValidation} from '../middlewares/urlsMiddleware.js'
-import {postShorten} from '../controllers/urlsControllers.js'
+import {postShorten,getUrlById} from '../controllers/urlsControllers.js'
 
 const router=express.Router();
 
 router.post('/urls/shorten',authUser,urlValidation,postShorten)
-
+router.get('/urls/:id',getUrlById)
 export default router;
